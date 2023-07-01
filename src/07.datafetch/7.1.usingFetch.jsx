c5cc8c5d68f6b568700a3d055a51ee0b238471ec
s-k-sahu <s.k.sahu01354@gmail.com>
2023-07-01 21:50:38 +0530

    api calling

--- src/07.datafetch/7.1.usingFetch.jsx
+++ src/07.datafetch/7.1.usingFetch.jsx
@@ -1,43 +1,89 @@
 import React, { Component } from "react";
-import { BASE_URL, POSTS } from "../services/constant";
+
 class UsingFetch extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      posts: [],
+      products: [],
+      // products: [
+      //   {
+      //     id: 1,
+      //     name: "samsung",
+      //     price: "200",
+      //   },
+      //   {
+      //     id: 2,
+      //     name: "nokia",
+      //     price: 300,
+      //   },
+      // ],
     };
   }
 
   componentDidMount() {
-    console.log("component did mount is calling..");
-    fetch(`${BASE_URL}${POSTS}`)
+    fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
-      .then((json) =>
+      .then((data) => {
         this.setState({
           ...this.state,
-          posts: json,
-        })
-      );
+          products: data,
+        });
+      });
+    // console.log("componentDidMount method is calling..");
   }
 
   render() {
-    const { posts } = this.state;
-    console.log(posts);
-    console.log("render method is calling...");
+    let USDollar = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "INR",
+    });
+    const { products } = this.state;
+    console.log("render method is calling..");
+    console.log(products);
     return (
-      <div className="container" style={{display:'flex',flexWrap:'wrap'}}>
-        {Array.isArray(posts) &&
-          posts.map((elm) => (
-            <div className="card" style={{margin:'10px',padding:'15px'}}>
-                <div>userId : {elm.userId}</div>
-                <hr />
-                <div>title : {elm.title}</div>
-                <hr />
-                <div>body : {elm.body}</div>
-            </div>
-          ))}
-      </div>
+      <>
+        <h2>using fetch method api call</h2>
+        <h4>console.log('xyz')</h4>
+        <h4>console.log(products)</h4>
+        {/* {
+          console.log(products)
+        } */}
+        {Array.isArray(products) &&
+          products.map((value) => {
+            const {
+              id,
+              category,
+              description,
+              image,
+              title,
+              price,
+              rating: { rate, count },
+            } = value;
+            return (
+              <div key={id} className="d-flex card p-4 m-2 md-3">
+                <div>
+                  <h1>{title}</h1>
+                  <img src={image} alt="img" height={100} width={100} />
+                </div>
+                <div className="fs-3 text-uppercase">{category}</div>
+                <p className="text-danger">{description}</p>
+                <ul>
+                  <li>price :{USDollar.format(price)}</li>
+                  <li>rating rate: {rate}</li>
+                  <li>rating count: {count}</li>
+                </ul>
+              </div>
+            );
+          })}
+        {/* {
+          Array.isArray(products) && products.map((value)=>(
+            <div>{value.name}</div>
+          ))
+        } */}
+        {/* {Array.isArray(products) &&
+          products.map(({ name, id, price }) => <div>{name}</div>)} */}
+      </>
     );
   }
 }
